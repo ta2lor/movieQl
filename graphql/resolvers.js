7e7fbf6b68e245145787091a0301e629c8f477c6
ta2lor <ksh262011@naver.com>
2021-05-09 14:26:41 +0900

    not good

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,14 @@
+import { getMovies, getById } from "./db";
+
 const resolvers = {
   Query: {
-    name: () => "hyoseok",
+    movies: () => getMovies,
+    movie: (_, { id }) => getById(id),
+
   },
+  Mutation:{
+    addMovie(_, {name, score}) 
+  }
 };
 
 export default resolvers;
